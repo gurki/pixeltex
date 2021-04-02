f0239e525f44335fe140e7d877cc5acc159624c3
Tobias Gurdan <tobias@gurdan.de>
2021-04-02 13:38:49 +0200

    Extend currency with crypto symbols and add 5x5 support for zcash

--- minigent/render.js
+++ minigent/render.js
@@ -10,8 +10,9 @@ export function drawLetter( canvas, letter ) {
 
     const cols = (
         ( letter.bits.length < 12 ) ? 2 :   //  2x4, 2x5
-        ( letter.bits.length === 16 ) ? 4 : //  4x4 arrows, at, nparallel
-        ( letter.bits.length === 20 ) ? 5 : //  5x4 infinity
+        ( letter.bits.length === 16 ) ? 4 : //  4x4 arrows
+        ( letter.bits.length === 20 ) ? 5 : //  5x4 infinity, crypto
+        ( letter.bits.length === 25 ) ? 5 : //  5x5 crypto
         3                                   //  3x4, 3x5
     );
     const rows = letter.bits.length / cols;
--- pixeltex/rasterizer.js
+++ pixeltex/rasterizer.js
@@ -132,7 +132,8 @@ function rasterizeToken( token ) {
     const cols = (
         ( letter.bits.length < 12 ) ? 2 :   //  2x4, 2x5
         ( letter.bits.length === 16 ) ? 4 : //  4x4 arrows
-        ( letter.bits.length === 20 ) ? 5 : //  5x4 infinity
+        ( letter.bits.length === 20 ) ? 5 : //  5x4 infinity, crypto
+        ( letter.bits.length === 25 ) ? 5 : //  5x5 crypto
         3                                   //  3x4, 3x5
     );
 
